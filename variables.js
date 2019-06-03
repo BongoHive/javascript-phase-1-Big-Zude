4cc87713a4d076406c54a59c938dd346f9492dad
Big Zude <zudemwango@gmail.com>
2019-06-04 01:20:45 +0200

    updated variables.js

--- variables.js
+++ variables.js
@@ -1,1 +1,14 @@
-// you solution should be here
+// 1
+var birthYear=1991;
+var futureYear=2091;
+age=futureYear-birthYear;
+console.log(age);
+console.log(age-1);
+
+
+//2
+var currentAge=27;
+var maxAge=150;
+var amountPayDay=4;
+var total=(maxAge-currentAge)*(365*amountPayDay);
+console.log(`You will need ${total} to last you until the ripe old age of ${maxAge}`);
